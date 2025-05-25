8d86c6f67f7c18aeb95d365c291af8881f918190
tejatayi <saisivatejatayi@gmail.com>
2025-05-25 16:18:33 -0400

    making changes

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    host: "0.0.0.0", // 👈 allows access over local network
+    port: 5173, // optional, makes sure port stays same
+  },
   base: "/portfolio-frontend/",
   plugins: [react()],
 });
